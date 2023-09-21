bcff4b14803e57e4e1489c7dd0d406bc1b13c734
gh1928 <oos1928@gmail.com>
2023-09-21 09:31:50 +0900

    퍼즐 스타트포인트 버튼 색들 수정

--- Assets/Working/Script/CafeTerrace/PuzzlePlayer.cs
+++ Assets/Working/Script/CafeTerrace/PuzzlePlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PuzzlePlay : MonoBehaviour
+public class PuzzlePlayer : MonoBehaviour
 {
     public GameObject checkPoint;
     public Image line;
