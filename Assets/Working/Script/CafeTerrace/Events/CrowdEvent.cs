5cf36f2e7cd04f4f023fbea5f7fcd0c7cfdfbfb4
gh1928 <oos1928@gmail.com>
2023-10-24 17:43:58 +0900

    군중 투명화 조절 기능 추가

--- Assets/Working/Script/CafeTerrace/Events/CrowdEvent.cs
+++ Assets/Working/Script/CafeTerrace/Events/CrowdEvent.cs
@@ -4,17 +4,64 @@ using UnityEngine;
 
 public class CrowdEvent : CaffeEventDeafault
 {
-    public Renderer crowdRenderer;
+    public GameObject[] crowds;
+    public float opacityChangeTime = 3f;
 
-    // Start is called before the first frame update
-    void Start()
+    private List<Material> crowdsMaterials = new List<Material>();
+    private void Start()
     {
-        
+        for(int i = 0; i < crowds.Length; i++)
+        {
+            AddMaterialsToList(crowds[i], crowdsMaterials);
+        }
+    }
+    private void AddMaterialsToList(GameObject go,List<Material> materials)
+    {
+        var renders = go.GetComponentsInChildren<Renderer>();
+
+        for(int i = 0; i < renders.Length;i++)
+        {
+            materials.AddRange(renders[i].materials);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeCrowdsOpacity(float opacity)
     {
-        
+        for(int i = 0; i < crowdsMaterials.Count; i++)
+        {
+            var color = crowdsMaterials[i].color;
+            color.a = opacity;
+            crowdsMaterials[i].color = color;
+        }
     }
+    private void ChangeMaterialsSurfaceType(float type)
+    {
+        for (int i = 0; i < crowdsMaterials.Count; i++)
+        {
+            crowdsMaterials[i].SetFloat("_Surface", type);
+        }
+    }
+    public override void OnEvent(List<PuzzleElement> _)
+    {
+        StartCoroutine(CrowdFadeIn());
+    }
+
+    private IEnumerator CrowdFadeIn()
+    {
+        float timer = 0f;
+        float reverseChangeTime = 1 / opacityChangeTime;
+
+        ChangeMaterialsSurfaceType(1f);
+
+        while (timer < opacityChangeTime)
+        {
+            ChangeCrowdsOpacity(timer * opacityChangeTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        ChangeMaterialsSurfaceType(0f);
+        ChangeCrowdsOpacity(1f);
+    }
+
 }
