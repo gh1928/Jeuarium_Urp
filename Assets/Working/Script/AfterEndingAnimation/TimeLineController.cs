e2444cbb5b0b075c0e6e815646d80b446af426e6
gh1928 <oos1928@gmail.com>
2023-11-14 10:15:19 +0900

    타임라인 애니메이터 추가

---
+++ Assets/Working/Script/AfterEndingAnimation/TimeLineController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineController : MonoBehaviour
+{
+    private PlayableDirector playableDirector;
+
+    private void Awake()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+}
