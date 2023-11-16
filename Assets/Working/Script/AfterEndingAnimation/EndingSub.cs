d0df529bff5d75e8baf94809601c99e32373a41d
gh1928 <oos1928@gmail.com>
2023-11-16 11:05:19 +0900

    자막 추가

---
+++ Assets/Working/Script/AfterEndingAnimation/EndingSub.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndingSub : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TextMeshProUGUI textMesh;
+
+    public BNG.ScreenFader screenFader;
+
+    public float fadeTime = 1f;
+
+    public string[] subs;
+    private int idx = 0;
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    public void DoFadeIn()
+    {
+        screenFader.DoFadeIn();
+        StartCoroutine(TextFadeCoroutine(true));
+
+        textMesh.text = subs[idx++];
+    }
+    public void DoFadeOut()
+    {
+        screenFader.DoFadeOut();
+        StartCoroutine(TextFadeCoroutine(false));
+    }
+    IEnumerator TextFadeCoroutine(bool fadeIn)
+    {
+        float timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            float value = timer / fadeTime;
+            canvasGroup.alpha = fadeIn ? value : 1f - value;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = fadeIn ? 1f : 0.0f;
+    }
+}
